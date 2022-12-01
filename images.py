67b6f0e65486c55d653e99fa54a1dff2e7b3342b
Matt Dowell <matt.dowell@sproutsocial.com>
2022-12-01 09:11:02 -0800

    Resize images

---
+++ images.py
@@ -0,0 +1,32 @@
+import os
+import sys
+
+from PIL import Image
+
+
+ARTICLE_IMAGE_WIDTH = 2240
+ARTICLE_IMAGE_HEIGHT = 1260
+RESIZED_FILENAME_SUFFIX = "-resized"
+
+
+def run():
+    images_folder = sys.argv[1] if len(sys.argv) > 1 else None
+    if images_folder is None or not os.path.isdir(images_folder):
+        print("Must supply image folder")
+        return
+    images_folder_full_path = os.path.abspath(images_folder)
+    files = os.listdir(images_folder_full_path)
+    for file in files:
+        if not (file.endswith(".png") or file.endswith(".jpeg") or file.endswith(".jpg")):
+            print(f"Skipping file {file}")
+            continue
+        with Image.open(os.path.join(images_folder_full_path, file)) as im:
+            (width, height) = (ARTICLE_IMAGE_WIDTH, int(im.height * (ARTICLE_IMAGE_WIDTH / im.width)))
+            im_resized = im.resize((width, height))
+        new_filename = "".join(file.split('.')[:-1]) + RESIZED_FILENAME_SUFFIX + "." + file.split('.')[-1]
+        im_resized.save(os.path.join(images_folder_full_path, new_filename))
+        print(f"Resized {file} and saved as {new_filename}")
+
+
+if __name__ == "__main__":
+    run()
