f6ac40671c3c9643e097ca765ed64e044333d0d9
Matt Dowell <matt.dowell@sproutsocial.com>
2024-06-15 18:29:37 -0700

    Start of oxbow article

--- new_article.py
+++ new_article.py
@@ -9,7 +9,7 @@ import os
 import subprocess
 
 
-ARTICLE_FOLDER = './articles/neighborhood/fresh_flours_05_24'
+ARTICLE_FOLDER = './articles/neighborhood/oxbow_06_24'
 
 ARTICLE_TEMPLATE = './templates/article.html'
 PARAGRAPH_TEMPLATE = './templates/paragraph.html'
