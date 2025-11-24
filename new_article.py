9b025fc192db788789a6825262e38658c4b9048a
Matt Dowell <mattdowell12@gmail.com>
2025-11-24 12:08:23 -0800

    Warm bun

--- new_article.py
+++ new_article.py
@@ -9,7 +9,7 @@ import os
 import subprocess
 
 
-ARTICLE_FOLDER = './articles/neighborhood/seasmith_10_25'
+ARTICLE_FOLDER = './articles/outdoors/warm_bun_11_25'
 
 ARTICLE_TEMPLATE = './templates/article.html'
 PARAGRAPH_TEMPLATE = './templates/paragraph.html'
